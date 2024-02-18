d0655a78ff4e2d31a404dfea3c3c6ad0d3352a19
floreesleo <florees.leo@gmail.com>
2024-02-18 16:52:40 -0600

    feat: :sparkles: Renderizar tareas
    Mostrar cartas de tareas por medio de una peticion a prisma

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,31 @@
-import React from "react";
+import { prisma } from "@/libs/prisma";
 
-export default function HomePage() {
-  return <div>Tareas</div>;
+async function loadTasks() {
+  // const res = await fetch("http://localhost:3000/api/tasks");
+  // const data = await res.json();
+
+  return await prisma.task.findMany();
+}
+
+export default async function HomePage() {
+  const tasks = await loadTasks();
+  console.log(tasks);
+  return (
+    <section className="container mx-auto">
+      <div className="grid grid-cols-3 gap-3 mt-10">
+        {tasks.map((task) => (
+          <div
+            key={task.id}
+            className="bg-blue-600 p-3 rounded hover:bg-blue-800 hover:cursor-pointer"
+          >
+            <h3 className="font-bold text-2xl mb-2">{task.title}</h3>
+            <p>{task.description}</p>
+            <p className="text-end">
+              {new Date(task.created_at).toLocaleDateString()}
+            </p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
